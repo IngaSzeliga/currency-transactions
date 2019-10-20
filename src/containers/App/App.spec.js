87c67d300a6b36971d02e2dfab2eab7af19a87e9
Inga Szeliga <inga.szeliga@gmail.com>
2019-10-20 22:48:21 +0200

    Added unit tests for App component

---
+++ src/containers/App/App.spec.js
@@ -0,0 +1,45 @@
+import React from "React";
+import { shallow, mount } from "enzyme";
+import App from ".";
+
+describe("App component", () => {
+  const component = shallow(<App />);
+
+  it("has working component", () => {
+    expect(component).toBeDefined();
+  });
+
+  it("expects component to never be changed", () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it("expects to call deleteTransaction", () => {
+    const componentMounted = mount(<App />);
+    componentMounted.setState({
+      transactions: [
+        {
+          id: "2caf5711-2913-4e41-95db-088625acf1bf",
+          title: "Name",
+          amount: 13,
+          convertedAmount: "54,99 zł"
+        }
+      ]
+    });
+    componentMounted
+      .instance()
+      .deleteTransaction("2caf5711-2913-4e41-95db-088625acf1bf");
+    expect(componentMounted.state("transactions")).toEqual([]);
+  });
+
+  it("expects to call addTransaction", () => {
+    const componentMounted = mount(<App />);
+    const transaction = {
+      id: "2caf5711-2913-4e41-95db-088625acf1bf",
+      title: "Name",
+      amount: 13,
+      convertedAmount: "54,99 zł"
+    };
+    componentMounted.instance().addTransaction(transaction);
+    expect(componentMounted.state("transactions")).toEqual([transaction]);
+  });
+});
